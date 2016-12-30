5857b9fe9d89012c2ef392fddf6aae1412010914
Logan Talbot <logandavidtalbot@gmail.com>
2016-12-30 16:28:52 +0000

    Intermediate Redux: Middleware

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const API_KEY = '6ff3659df41f6e70e6b45937995a7b6d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
